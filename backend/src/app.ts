6d889da6fe8462c31aff2cd1c69e911b2d947873
Taha <taha@Tahas-MacBook-Air.local>
2026-01-13 10:06:14 +0530

    Refactor: Backend cors frontend link

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,15 +1,27 @@
 import express from "express";
 import cors from "cors";
 import router from "./route.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
-app.use(cors({ origin: "https://mini-freelance-marketplace-9j4e.vercel.app", credentials: true }));
+app.use(
+  cors({
+    origin: process.env['CLIENT_URL'],
+    credentials: true
+  })
+);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (_req, res) => {
-  res.json({ status: "ok", service: "Mini Freelance Marketplace API" });
+  res.json({
+    status: "ok",
+    service: "Mini Freelance Marketplace API"
+  });
 });
 
 app.use("/api", router);
